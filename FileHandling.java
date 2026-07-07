fbb71d5ddc069ca3969165aa01f662d3f870e324
Neelu Chauhan <Neelusinghchauhan1996@gmail.com>
2026-07-07 19:59:02 +0530

    Writing numbers in the file

--- FileHandling.java
+++ FileHandling.java
@@ -3,28 +3,16 @@ import java.io.*;
 
 public class FileHandling {
     public static void main(String[] args) {
-
         File file = new File("name.txt");
         try {
-            BufferedReader bf = new BufferedReader(new FileReader("name.txt"));
-            String line;
-            String longestLine= "";
-            int length=0;
-            while ((line = bf.readLine()) != null) {
-                if (line.length()>length) {
-                    longestLine= line;
-                    length= line.length();
-                }
+            BufferedWriter bw= new BufferedWriter(new FileWriter("name.txt", true));
+            for (int i=1;i<=100;i++){
+                bw.write(String.valueOf(i));
+                bw.newLine();
             }
-            System.out.println("Longest line is " +longestLine);
-            System.out.println("It's length is: " +length);
-            bf.close();
-        }
-        catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            bw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
