974ebe15aa8b9a2cc5c8eba35629a20ecfe27776
Neelu Chauhan <Neelusinghchauhan1996@gmail.com>
2026-07-08 18:21:24 +0530

    Copy data from one file to another

--- FileHandling.java
+++ FileHandling.java
@@ -6,20 +6,20 @@ public class FileHandling {
     public static void main(String[] args) {
         File file = new File("name.txt");
         try {
-            Scanner sc= new Scanner(file);
-            while (sc.hasNext()){
-                if (sc.hasNextInt()) {
-                    int num = sc.nextInt();
-                    System.out.println(num);
+            BufferedReader br= new BufferedReader(new FileReader("name.txt"));
+                BufferedWriter bw= new BufferedWriter(new FileWriter("copy.txt"));
+                String line;
+                while ((line= br.readLine())!=null){
+                    bw.write(line);
+                    bw.newLine();
                 }
-                else{
-                    sc.next();
-                }
-            }
-
-            sc.close();
+                bw.close();
+                br.close();
         } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+            throw new RuntimeException(e);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
